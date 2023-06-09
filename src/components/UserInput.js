95f7b8b4d8bfb846bfded74aaf3bbe6ad1f54365
Emdadul_Islam <emdadulislam439@gmail.com>
2023-06-09 07:28:30 +0600

    added style into Input and Output components

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -3,7 +3,19 @@ import React from "react";
 class UserInput extends React.Component {
   render() {
     return (
-      <div>
+      <div
+        style={{
+          width: "30%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "5px",
+          marginBottom: "5px",
+          border: "1px solid #ccc",
+          boxShadow: "1px 2px 2px #ccc",
+          borderRadius: "10px",
+          textAlign: "center",
+        }}
+      >
         <div>
           <h1>UserInput:</h1>
         </div>
@@ -11,6 +23,7 @@ class UserInput extends React.Component {
           type="textInput"
           onChange={this.props.onChange}
           value={this.props.username}
+          style={{ marginBottom: "30px" }}
         />
       </div>
     );
--- src/components/UserOutput.js
+++ src/components/UserOutput.js
@@ -3,7 +3,18 @@ import React from "react";
 class UserOutput extends React.Component {
   render() {
     return (
-      <div>
+      <div
+        style={{
+          width: "30%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "5px",
+          marginBottom: "5px",
+          border: "1px solid #ccc",
+          borderRadius: "10px",
+          boxShadow: "1px 2px 2px #ccc",
+        }}
+      >
         <div>
           <h1>UserOutput:</h1>
         </div>
