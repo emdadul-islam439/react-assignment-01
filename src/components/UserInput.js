1f5949f36329980ddccf15ecc58423ee2df0ec53
Emdadul_Islam <emdadulislam439@gmail.com>
2023-06-09 07:45:12 +0600

    added style from stylesheet

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,21 +1,10 @@
 import React from "react";
+import "./UserInput.css";
 
 class UserInput extends React.Component {
   render() {
     return (
-      <div
-        style={{
-          width: "30%",
-          marginLeft: "auto",
-          marginRight: "auto",
-          marginTop: "5px",
-          marginBottom: "5px",
-          border: "1px solid #ccc",
-          boxShadow: "1px 2px 2px #ccc",
-          borderRadius: "10px",
-          textAlign: "center",
-        }}
-      >
+      <div className="UserInput">
         <div>
           <h1>UserInput:</h1>
         </div>
