cd1427d413898aa47ffaa257e989fb261df8f50f
Emdadul_Islam <emdadulislam439@gmail.com>
2023-06-09 06:30:19 +0600

    added state to save username, and passed the saved-state into UserOutput component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import UserInput from "./components/UserInput";
 import UserOutput from "./components/UserOutput";
 
 function App() {
+  const [username, setUsername] = useState("Emdadul Islam");
   return (
     <div className="App">
       <UserInput />
-      <UserOutput username="Emdad" />
-      <UserOutput username="Sajid" />
-      <UserOutput username="Sakib" />
+      <UserOutput username={username} />
+      <UserOutput username={username} />
+      <UserOutput username={username} />
     </div>
   );
 }
