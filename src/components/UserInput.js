145b5cbac204c34c19079806286546e1417b25c5
Emdadul_Islam <emdadulislam439@gmail.com>
2023-06-09 06:14:59 +0600

    created two components and added different element into them

---
+++ src/components/UserInput.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class UserInput extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>UserInput:</h1>
+        </div>
+        <input type="textInput" />
+      </div>
+    );
+  }
+}
+
+export default UserInput;
---
+++ src/components/UserOutput.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class UserOutput extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>UserOutput:</h1>
+        </div>
+        <p>loremp ipsum 1</p>
+        <p>loremp ipsum 2</p>
+      </div>
+    );
+  }
+}
+
+export default UserOutput;
